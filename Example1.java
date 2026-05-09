c2da8cc2807ccd8feef2a2aa0f9904e690587fc5
Onkar Kokane <onkarkokane3@gmail.com>
2026-05-09 14:33:45 +0530

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Example1{
+	public static void main(String[]args){
+	Scanner sc=new Scanner(System.in);
+
+	System.out.println("enter a number");
+	int num=sc.nextInt();
+	System.out.println("Entered Number is"+num);
+	int i=1;
+
+	while(i<=num){
+		System.out.println(i);
+		i++;
+
+	}
+
+
+
+	}
+	
+	
+}
